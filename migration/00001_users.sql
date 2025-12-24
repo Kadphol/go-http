906639094aa6b83ab43f891ba7ce55d903de966b
Mark-Kadphol Lenglerdphol <kadphol.l@7solutions.co.th>
2025-12-24 16:51:44 +0700

    add migration

---
+++ migration/00001_users.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users (
+	id BIGSERIAL PRIMARY KEY,
+	username VARCHAR(255) UNIQUE NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
