de0efb10f6df6fb8502493a7a0d2dcb62789f914
Mark-Kadphol Lenglerdphol <kadphol.l@7solutions.co.th>
2025-12-24 14:24:21 +0700

    add pgsql connection

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,27 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/Kadphol/go-http/internal/api"
+	"github.com/Kadphol/go-http/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
 	logger := log.New(os.Stdout, "app: ", log.Ldate|log.Ltime)
 
 	//out store will go here
@@ -24,6 +31,7 @@ func NewApplication() (*Application, error) {
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 	return app, nil
 }
